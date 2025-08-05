0a975999b87269476705f89a9426b443f6b5099c
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-08-06 02:25:55 +0300

    bugfix(item respawn and health kit animation): items respawn on both server and client, health packs have basic animation

--- Assets/Scripts/PickUp/PickUpObject.cs
+++ Assets/Scripts/PickUp/PickUpObject.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class PickUpObject : NetworkBehaviour
 {
-    [SerializeField] private bool yRotation = true;
-    [SerializeField] private bool yMove = true;
-    [SerializeField] private float minHeight = 0.5f;
-    [SerializeField] private float maxHeight = 1.5f;
-    [SerializeField] private float rotateSpeed= 100f;
+    // [SerializeField] private bool yRotation = true;
+    // [SerializeField] private bool yMove = true;
+    // [SerializeField] private float minHeight = 0.5f;
+    // [SerializeField] private float maxHeight = 1.5f;
+    // [SerializeField] private float rotateSpeed= 100f;
     protected bool itemPickedUp = false; 
     public override void OnStartClient()
     {
@@ -38,17 +38,17 @@ public class PickUpObject : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (yMove) { // item oscilates up and down
-            float t = Mathf.PingPong(Time.time, 1f); // Goes from 0 to 1 and back
-            float easedT = Mathf.SmoothStep(0f, 1f, t); // Eases at ends
-            float y = Mathf.Lerp(minHeight, maxHeight, easedT);
-            transform.position = new Vector3(transform.position.x, y, transform.position.z);
-        }
-        if (yRotation)
-        { // item rotates around the y axis
-            Vector3 rotation = Vector3.zero;
-            rotation += Vector3.forward;
-            transform.Rotate(rotation * rotateSpeed * Time.deltaTime);
-        }
+        // if (yMove) { // item oscilates up and down
+        //     float t = Mathf.PingPong(Time.time, 1f); // Goes from 0 to 1 and back
+        //     float easedT = Mathf.SmoothStep(0f, 1f, t); // Eases at ends
+        //     float y = Mathf.Lerp(minHeight, maxHeight, easedT);
+        //     transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        // }
+        // if (yRotation)
+        // { // item rotates around the y axis
+        //     Vector3 rotation = Vector3.zero;
+        //     rotation += Vector3.forward;
+        //     transform.Rotate(rotation * rotateSpeed * Time.deltaTime);
+        // }
     }
 }
