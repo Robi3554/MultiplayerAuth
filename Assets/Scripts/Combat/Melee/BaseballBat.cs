d35e26dd4afe75d9a370d9376e4fa489a9b7750c
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-08-18 19:56:01 +0300

    pick-up system bugged and basic melee implemented

---
+++ Assets/Scripts/Combat/Melee/BaseballBat.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseballBat", menuName = "Scriptable Objects/BaseballBat")]
+public class BaseballBat : BaseMeleeWeapon
+{
+    //de adaugat vfx sau sfx specifice armei aici sau whatever e specific
+}
