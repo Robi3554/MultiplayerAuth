848d1a105112749c92da06205ff08421f8593c3f
Robert Varga <rvarga253@yahoo.com>
2025-08-26 20:55:16 +0300

    player HUD doesn't replicate

--- Assets/_Scripts/PlayerScripts/PlayerNetworkInitializer.cs
+++ Assets/_Scripts/PlayerScripts/PlayerNetworkInitializer.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class PlayerNetworkInitializer : NetworkBehaviour
 {
+    [SerializeField]
+    private GameObject playerHUD;
+
     public override void OnStartServer()
     {
         base.OnStartServer();
@@ -10,13 +13,19 @@ public class PlayerNetworkInitializer : NetworkBehaviour
         int clientId = (int)Owner.ClientId;
         var stats = GetComponent<PlayerStats>();
 
-        Debug.Log($"[OnStartServer] Registering player with ClientId: {clientId}");
-
         PlayerManager.Instance.players[clientId] = new PlayerManager.Player
         {
             playerObject = this.gameObject,
             connection = Owner,
             stats = stats
         };
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if (!IsOwner)
+            playerHUD.SetActive(false);
     }
 }
