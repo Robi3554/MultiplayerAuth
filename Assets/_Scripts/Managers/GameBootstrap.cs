f76e7ce368f22d28617fc05d616170543d8728c7
dcarpencu <dcarpencu@gmail.com>
2025-09-25 14:46:53 +0300

    Android support

--- Assets/_Scripts/Managers/GameBootstrap.cs
+++ Assets/_Scripts/Managers/GameBootstrap.cs
@@ -89,3 +89,87 @@ public class NetworkBootstrap : MonoBehaviour
         return (address, port);
     }
 }
+/*
+ * using UnityEngine;
+using FishNet.Managing;
+using FishNet.Transporting.Tugboat;
+using TMPro;
+using UnityEngine.UI;
+
+public class NetworkBootstrap : MonoBehaviour
+{
+    private NetworkManager _networkManager;
+    private Tugboat _tugboat;
+
+    [Header("UI References")]
+    [SerializeField] private GameObject _connectionPanel;
+    [SerializeField] private TMP_InputField _addressInput;
+    [SerializeField] private Button _connectButton;
+
+    private void Awake()
+    {
+        _networkManager = FindFirstObjectByType<NetworkManager>();
+        _tugboat = _networkManager.GetComponent<Tugboat>();
+    }
+
+    private void Start()
+    {
+        #if UNITY_EDITOR
+            if (ParrelSync.ClonesManager.IsClone())
+                _networkManager.ClientManager.StartConnection();
+            else
+            {
+                _networkManager.ServerManager.StartConnection();
+                _networkManager.ClientManager.StartConnection();
+            }
+        #elif DEDICATED_SERVER
+            _networkManager.ServerManager.StartConnection();
+        #elif CLIENT
+            // Subscribe to the correct connection events
+            _networkManager.ClientManager.OnAuthenticated += OnAuthenticated; // Corrected event
+            _networkManager.ClientManager.OnClientStopped += OnClientStopped;
+
+            _connectionPanel.SetActive(true);
+            _connectButton.onClick.AddListener(OnConnectClicked);
+        #else
+            _networkManager.ServerManager.StartConnection();
+            _networkManager.ClientManager.StartConnection();
+        #endif
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe from events to prevent memory leaks
+        if (_networkManager != null)
+        {
+            _networkManager.ClientManager.OnAuthenticated -= OnAuthenticated; // Corrected event
+            _networkManager.ClientManager.OnClientTimeOut -= OnClientTimeOut;
+        }
+    }
+
+    // Called by the UI button click
+    public void OnConnectClicked()
+    {
+        string address = string.IsNullOrWhiteSpace(_addressInput.text) ? "localhost" : _addressInput.text;
+        _tugboat.SetClientAddress(address);
+        _networkManager.ClientManager.StartConnection();
+    }
+
+    // This event runs when the client successfully connects and is authenticated
+    private void OnAuthenticated()
+    {
+        Debug.Log("Client successfully authenticated.");
+        _connectionPanel.SetActive(false);
+    }
+
+    // This event runs if the client disconnects or fails to connect
+    private void OnClientTimeOut()
+    {
+        Debug.Log("Client disconnected or failed to connect.");
+        if (_connectionPanel != null)
+        {
+            _connectionPanel.SetActive(true);
+        }
+    }
+}
+*/
