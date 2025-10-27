acf1041ac306f1cf31b2b94495191888c849be4f
dcarpencu <dcarpencu@gmail.com>
2025-10-27 21:25:08 +0200

    Freestyle character and bullets

---
+++ Assets/_Scripts/Camera/ChangePerspective.cs
@@ -0,0 +1,24 @@
+// using Unity.Cinemachine;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+//
+// public class ChangePerspective : MonoBehaviour
+// {
+//     private bool _perspective;
+//     
+//     [SerializeField]
+//     private CinemachineCamera _camera;
+//     
+//     public void OnPerspective(InputAction.CallbackContext context)
+//     {
+//         _perspective = context.ReadValueAsButton();
+//     }
+//     void Update()
+//     {
+//         if (_perspective)
+//         {
+//             _camera.Follow = 
+//         }
+//         
+//     }
+// }
