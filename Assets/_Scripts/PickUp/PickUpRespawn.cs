1e1eab5d5050ceba086cda4fbe5011660d4e78e4
bagaretk <96477537+bagaretk@users.noreply.github.com>
2025-11-14 01:30:08 +0200

    Pickup System fix & Medkit fix (#29)
    * PickupSystem: Healthkits work on parrelsync
    
    * PickUpSystem: works on server and cleanup

--- Assets/_Scripts/PickUp/PickUpRespawn.cs
+++ Assets/_Scripts/PickUp/PickUpRespawn.cs
@@ -3,17 +3,22 @@ using FishNet.Object;
 using System.Collections.Generic;
 using FishNet.Object.Synchronizing;
 using FishNet.Connection;
+using UnityEngine.UIElements;
 
 public class PickUpRespawn : NetworkBehaviour
 {
+    [SerializeField] private GameObject spawnableObjPrefab;
     [SerializeField] private float respawnTime = 10f;
-    private readonly SyncList<RespawnData> _respawnList = new SyncList<RespawnData>();
 
+    private readonly List<RespawnData> _respawnList = new List<RespawnData>();
+    
     [System.Serializable]
     private struct RespawnData
     {
-        public NetworkObject childNetworkObject;
+        public NetworkObject pickedNetObj;
         public float respawnTimer;
+        public Vector3 position;
+        public Quaternion rotation;
     }
 
     [Server]
@@ -28,70 +33,25 @@ public class PickUpRespawn : NetworkBehaviour
             Debug.LogWarning($"Index {index} is out of range for _respawnList (Count: {_respawnList.Count})");
         }
     }
-    private void _myCollection_OnChange(SyncListOperation op, int index,
-        RespawnData oldItem, RespawnData newItem, bool asServer)
-    {
-        switch (op)
-        {
-            /* An object was added to the list. Index
-            * will be where it was added, which will be the end
-            * of the list, while newItem is the value added. */
-            case SyncListOperation.Add:
 
-                Debug.Log($"Item added at index {index}");
-                break;
-            /* An object was removed from the list. Index
-            * is from where the object was removed. oldItem
-            * will contain the removed item. */
-            case SyncListOperation.RemoveAt:
-                Debug.Log($"Item removed from index {index}");
-                break;
-            /* An object was inserted into the list. Index
-            * is where the obejct was inserted. newItem
-            * contains the item inserted. */
-            case SyncListOperation.Insert:
-                break;
-            /* An object replaced another. Index
-            * is where the object was replaced. oldItem
-            * is the item that was replaced, while
-            * newItem is the item which now has it's place. */
-            case SyncListOperation.Set:
-                break;
-            /* All objects have been cleared. Index, oldValue,
-            * and newValue are default. */
-            case SyncListOperation.Clear:
-                break;
-            /* When complete calls all changes have been
-            * made to the collection. You may use this
-            * to refresh information in relation to
-            * the list changes, rather than doing so
-            * after every entry change. Like Clear
-            * Index, oldItem, and newItem are all default. */
-            case SyncListOperation.Complete:
-                break;
-        }
-    }
-    private void Awake()
-    {
-        /* Listening to SyncList callbacks are a
-        * little different from SyncVars. */
-        _respawnList.OnChange += _myCollection_OnChange;
-    }
     public override void OnStartServer()
     {
         base.OnStartServer();
     }
 
     // call this method to start the respawn timer in the item
-    // [ServerRpc(RequireOwnership = false)]
-    public void StartRespawnTimer(NetworkObject childNetObj)
+    public void StartRespawnTimer(NetworkObject pickedNetworkObj, Vector3 initPosition, Quaternion initRotation)
     {
         Debug.Log("PickUpRespawn: Starting respawn timer");
+        if (pickedNetworkObj == null || !IsServerStarted) return;
         RespawnData newRespawn = new RespawnData
         {
-            childNetworkObject = childNetObj,
-            respawnTimer = respawnTime
+            pickedNetObj = pickedNetworkObj,
+            respawnTimer = respawnTime,
+            position = initPosition,
+            rotation = initRotation
         };
+        ServerManager.Despawn(pickedNetworkObj.gameObject);
         _respawnList.Add(newRespawn);
     }
 
@@ -116,25 +76,18 @@ public class PickUpRespawn : NetworkBehaviour
                 ModifyRespawnData(i, data);
             }
         }
-
     }
-    // [ServerRpc(RequireOwnership = false)]
     private void RespawnChildServer(RespawnData data)
     {
-        Debug.Log("PickUpRespawn: Respawning child");
-        if (data.childNetworkObject == null)
+        Debug.Log("PickUpRespawn: Respawning child on server");
+        if (spawnableObjPrefab == null)
         {
-            Debug.LogWarning("Tried to respawn null object.");
+            Debug.LogError("PickUpRespawn: spawnableObjPrefab is null; cannot respawn.");
             return;
         }
-        RespawnChildObserver(data);
-
-        // ServerManager.Spawn(data.childNetworkObject);   
-    }
-    [ObserversRpc]
-    private void RespawnChildObserver(RespawnData data)
-    {
-        data.childNetworkObject.gameObject.SetActive(true);
-
+        
+        GameObject spawnedObj = Instantiate(spawnableObjPrefab, data.position, data.rotation);
+        spawnedObj.transform.SetParent(this.transform, true);
+        ServerManager.Spawn(spawnedObj);
     }
 }
