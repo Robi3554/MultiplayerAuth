ed9904f75c64d3dbb69b9bd11bc9c77c5dfba10c
dave <dcarpencu@gmail.com>
2026-01-25 23:20:38 +0200

    Fix for username input - NO MORE EMOJIS

--- Assets/_Scripts/Managers/MainMenuController.cs
+++ Assets/_Scripts/Managers/MainMenuController.cs
@@ -3,18 +3,67 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.Text.RegularExpressions;
 
 public class MainMenuController : MonoBehaviour
 {
+    private const int MaxUsernameLength = 20;
+    
     [SerializeField] private TMP_InputField _addressInput;
     [SerializeField] private TMP_InputField _usernameInput;
     [SerializeField] private string _gameSceneName = "SampleScene";
+
+    private void Start()
+    {
+        // Set character limit on the input field
+        if (_usernameInput != null)
+        {
+            _usernameInput.characterLimit = MaxUsernameLength;
+            _usernameInput.onValueChanged.AddListener(OnUsernameInputChanged);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_usernameInput != null)
+        {
+            _usernameInput.onValueChanged.RemoveListener(OnUsernameInputChanged);
+        }
+    }
+
+    private void OnUsernameInputChanged(string newValue)
+    {
+        // Sanitize in real-time as the user types
+        string sanitized = SanitizeUsername(newValue);
+        if (sanitized != newValue)
+        {
+            _usernameInput.text = sanitized;
+        }
+    }
+
+    /// <summary>
+    /// Sanitizes a username to only allow alphanumeric characters, max 20 chars.
+    /// </summary>
+    public static string SanitizeUsername(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return string.Empty;
+
+        // Remove all non-alphanumeric characters (letters and digits only)
+        string sanitized = Regex.Replace(input, @"[^a-zA-Z0-9]", "");
+
+        // Limit to max length
+        if (sanitized.Length > MaxUsernameLength)
+            sanitized = sanitized.Substring(0, MaxUsernameLength);
+
+        return sanitized;
+    }
 
     public void OnConnectClicked()
     {
         // Save the IP address to our persistent data holder.
         ConnectionInfo.IpAddress = "193.226.15.26";
-        ConnectionInfo.username = _usernameInput.text;
+        ConnectionInfo.username = SanitizeUsername(_usernameInput.text);
 
         // Load the game scene.
         SceneManager.LoadScene(_gameSceneName);
@@ -23,7 +72,7 @@ public class MainMenuController : MonoBehaviour
     public void OnLocalhostClicked()
     {
         ConnectionInfo.IpAddress = "localhost";
-        ConnectionInfo.username = _usernameInput.text;
+        ConnectionInfo.username = SanitizeUsername(_usernameInput.text);
         
         SceneManager.LoadScene(_gameSceneName);
     }
