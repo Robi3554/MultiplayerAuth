509a2ebe7904b28252c4f6a2ae4a94d91e1fe5b7
Robert Varga <rvarga253@yahoo.com>
2026-01-28 18:38:49 +0200

    Added limit to BIG HEAD and moved the medkits around

--- Assets/_Scripts/PlayerScripts/PlayerStats.cs
+++ Assets/_Scripts/PlayerScripts/PlayerStats.cs
@@ -34,6 +34,8 @@ public class PlayerStats : NetworkBehaviour
 
     private Slider healthSlider;
 
+    private bool isHeadBig;
+
     public bool isRespawning = false;
 
     public override void OnStartClient()
@@ -253,13 +255,20 @@ public class PlayerStats : NetworkBehaviour
 
     private IEnumerator ChangeHeadCo(Transform head, float multiplier)
     {
-        var originalScale = head.localScale;
+        if (!isHeadBig)
+        {
+            var originalScale = head.localScale;
 
-        head.localScale *= multiplier;
+            head.localScale *= multiplier;
+            
+            isHeadBig = true;
 
-        yield return new WaitForSeconds(10f);
+            yield return new WaitForSeconds(10f);
 
-        head.localScale = originalScale;
+            head.localScale = originalScale;
+
+            isHeadBig = false;
+        }
     }
     #endregion
 
@@ -278,12 +287,15 @@ public class PlayerStats : NetworkBehaviour
 
     private IEnumerator ChangeMultCo(int multiplier)
     {
-        int oldMultiplier = damageMult;
-        damageMult = multiplier;
+        if (isHeadBig)
+        {
+            int oldMultiplier = damageMult;
+            damageMult = multiplier;
 
-        yield return new WaitForSeconds(10f);
+            yield return new WaitForSeconds(10f);
 
-        damageMult = oldMultiplier;
+            damageMult = oldMultiplier;
+        }
     }
     #endregion
 }
