dbb2cfdd4e84e26f3fcb7f5482cf6f92eea50c78
dave <dcarpencu@gmail.com>
2026-05-09 16:53:28 +0300

    turret, please stop hitting urself

--- Assets/_Scripts/Combat/Projectile/ProjectileScript.cs
+++ Assets/_Scripts/Combat/Projectile/ProjectileScript.cs
@@ -73,7 +73,7 @@ public class ProjectileScript : NetworkBehaviour
             PlayerManager.Instance.DamagePlayer(targetId, finalDamage, attackerId);
             DespawnProjectile();
         }
-        else if (col.GetComponentInParent<Turret>() is Turret turret)
+        else if (attackerId >= 0 && col.GetComponentInParent<Turret>() is Turret turret)
         {
             turret.TakeDamage(finalDamage);
             DespawnProjectile();
